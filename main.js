37b122249a0394111a0bb71e300c4612863a8e01
Lucas Gonçalves <lucas.gabriel.goncalves12@gmail.com>
2023-02-09 19:32:00 -0300

    feat: added localstorage to save the theme

--- main.js
+++ main.js
@@ -2,11 +2,31 @@ const toggleTheme = document.getElementById('toggle-theme');
 const circleToggleTheme = document.querySelector('#toggle-theme .circle');
 const html = document.querySelector('html');
 
+
+function onLoadDocument() {
+  const theme = localStorage.getItem('@startec-portfolio:theme');
+
+  if (theme === 'dark') {
+    html.classList.add('dark');
+    circleToggleTheme.classList.add('active');
+  } else {
+    html.classList.remove('dark');
+    circleToggleTheme.classList.remove('active');
+  }
+}
+
 toggleTheme.addEventListener('click', function () {
   circleToggleTheme.classList.toggle('active');
-  html.classList.toggle('dark');
-});
 
+  html.classList.toggle('dark');
+
+  if (html.classList.contains('dark')) {
+    localStorage.setItem('@startec-portfolio:theme', 'dark');
+  } else {
+    localStorage.removeItem('@startec-portfolio:theme');
+  }
+
+});
 
 
 ScrollReveal().reveal('header', {
